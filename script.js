f36f93225515ecb59c0588d7d1c7932de43ee7a3
Arthur Magge <52111394+Artmr5555@users.noreply.github.com>
2019-10-01 19:09:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.getElementsByClassName('repostas1')
+    function apagar() {
+        document.getElementById('resp').style.display = 'none'
+    }
+    function timer() {
+        setTimeout(function(){document.getElementsByClassName('topic1')[0].style.display = 'none';
+        document.getElementsByClassName('topic2')[0].style.display = 'initial'}, 400)
+    }
+
+    function timer2() {
+        setTimeout(function(){document.getElementsByClassName('topic2')[0].style.display = 'none';
+        document.getElementsByClassName('topic3')[0].style.display = 'initial'}, 400)
+    }
+
